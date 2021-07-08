078d7f138ac1b8d9c7826d02f2af09b3ce0fbb54
Mesut Kaçmaz <mesut.kacmaz@outlook.com>
2021-07-08 23:57:07 +0300

    Added .env & connected to the database

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 const express = require('express')
 const products = require('./data/products')
+const dotenv = require('dotenv')
+const connectDB = require('./config/db')
+
+dotenv.config()
+
+connectDB()
 
 const app = express()
 
@@ -12,4 +18,6 @@ app.get('/api/products/:id', (req, res) => {
   res.json(product)
 })
 
-app.listen(5000, console.log('Server running on port 5000'))
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
