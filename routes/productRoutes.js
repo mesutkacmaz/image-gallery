bd3eef16e7094ae4b64c8e8a4667b192bb415b08
Mesut Kaçmaz <mesut.kacmaz@outlook.com>
2021-07-09 16:14:13 +0300

    Fetching products from the database

---
+++ routes/productRoutes.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const asyncHandler = require('express-async-handler')
+const router = express.Router()
+const Product = require('../models/Product')
+
+router.get('/', asyncHandler(async (req, res) => {
+  const products = await Product.find()
+  res.json(products)
+}))
+
+router.get('/:id', asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id)
+
+  if (product) {
+    res.json(product)
+  } else {
+    res.status(404).json({ message: 'Product not found' })
+  }
+}))
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express')
-const products = require('./data/products')
 const dotenv = require('dotenv')
 const connectDB = require('./config/db')
+
+const productRoutes = require('./routes/productRoutes')
 
 dotenv.config()
 
@@ -9,14 +10,7 @@ connectDB()
 
 const app = express()
 
-app.get('/api/products', (req, res) => {
-  res.json(products)
-})
-
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find(p => p._id === req.params.id)
-  res.json(product)
-})
+app.use('/api/products', productRoutes)
 
 const PORT = process.env.PORT || 5000
 
