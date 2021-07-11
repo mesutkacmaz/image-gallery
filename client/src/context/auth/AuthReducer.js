c8a53553a0c32aec0b856d5a6a7e71782c7d534a
Mesut Kaçmaz <mesut.kacmaz@outlook.com>
2021-07-11 18:15:11 +0300

    Some fixes on auth reducer

--- client/src/context/auth/AuthReducer.js
+++ client/src/context/auth/AuthReducer.js
@@ -3,37 +3,23 @@ import { REGISTER_SUCCESS, REGISTER_FAIL, LOGIN_SUCCESS, LOGIN_FAIL, LOGOUT, CLE
 const authReducer = (state, action) => {
   switch (action.type) {
     case REGISTER_SUCCESS:
+    case LOGIN_SUCCESS:
+      localStorage.setItem('token', action.payload.token)
       return {
         ...state,
+        ...action.payload,
         loading: false,
-        user: action.payload,
         isAuthenticated: true
       }
     case REGISTER_FAIL:
+    case LOGIN_FAIL:
+    case LOGOUT:
+      localStorage.removeItem('token')
       return {
         ...state,
-        loading: false,
-        error: action.payload
-      }
-    case LOGIN_SUCCESS:
-      return {
-        ...state, 
-        loading: false, 
-        user: action.payload,
-        isAuthenticated: true
-      }
-    case LOGIN_FAIL:
-      return {
-        ...state, 
-        loading: false, 
-        error: action.payload 
-      }
-    case LOGOUT:
-      localStorage.removeItem('user')
-      return {
+        token: null,
         isAuthenticated: false,
         loading: false,
-        user: null,
         error: action.payload
       }
     case CLEAR_ERRORS:
